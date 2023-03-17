0e504ef1df9e36c3e860fc5e4e4713f5d2404016
rentorious <rentorious@gmail.com>
2023-03-17 17:57:18 +0100

    register and login form

---
+++ client/src/scenes/loginPage/constants.js
@@ -0,0 +1,14 @@
+export const initialValuesRegister = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  location: "",
+  occupation: "",
+  picture: "",
+};
+
+export const initialValuesLogin = {
+  email: "",
+  password: "",
+};
