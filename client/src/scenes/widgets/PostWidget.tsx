b5e8260d41844258c14ddfbb34d7bfde898033f1
rentorious <rentorious@gmail.com>
2023-04-10 17:41:50 +0200

    FE: update picture paths to use CDN

--- client/src/scenes/widgets/PostWidget.tsx
+++ client/src/scenes/widgets/PostWidget.tsx
@@ -77,7 +77,7 @@ function PostWidget({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`${baseUrl}/assets/${picturePath}`}
+          src={picturePath}
         />
       )}
       <FlexBetween mt="0.25rem">
